2e47b66befe1d3370bfd718603108ad48f58e1e6
Anastasia <malevinsky@yandex-team.ru>
2021-02-07 23:45:39 +0300

    fixed

--- bank/bank/src/main/java/com/example/alpha/bank/controllers/MainController.java
+++ bank/bank/src/main/java/com/example/alpha/bank/controllers/MainController.java
@@ -25,66 +25,21 @@ import java.net.http.HttpResponse;
 
 @Controller
 public class MainController {
+    /**
+     * Я использовала две ссылки - за сегодня и за определенную дату. Чтобы узнать курс за вчера нужно вписать в POSTS_API_URL_YESTERDAY нужное число.
+     */
     public static final String POSTS_API_URL = "https://openexchangerates.org/api/latest.json?app_id=6e30f6727b474120b5816cef72283cfd";
     public static final String POSTS_API_URL_YESTERDAY = "https://openexchangerates.org/api/historical/2021-01-05.json?app_id=6e30f6727b474120b5816cef72283cfd";
 
-    public static int lala(float fin) throws IOException, InterruptedException {
-        HttpClient client = HttpClient.newHttpClient();
-        HttpRequest request = HttpRequest.newBuilder()
-                .GET()
-                .header("accept", "application/json")
-                .uri(URI.create(POSTS_API_URL))
-                .build();
-        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
-
-        //Считываем вчерашние JSON'ы.
-        HttpClient client_y = HttpClient.newHttpClient();
-        HttpRequest request_y = HttpRequest.newBuilder()
-                .GET()
-                .header("accept", "application/json")
-                .uri(URI.create(POSTS_API_URL_YESTERDAY))
-                .build();
-        HttpResponse<String> response_y = client_y.send(request_y, HttpResponse.BodyHandlers.ofString());
-
-        //Преобразуем  сегодняшние JSON'ы в JAVA объект.
-        ObjectMapper mapper = new ObjectMapper();
-        mapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
-        mapper.setVisibility(VisibilityChecker.Std.defaultInstance().withFieldVisibility(JsonAutoDetect.Visibility.ANY));
-        Post posts = mapper.readValue(response.body(), Post.class);
-
-        //Преобразуем  вчерашние JSON'ы в JAVA объект.
-        ObjectMapper mapper_y = new ObjectMapper();
-        mapper_y.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
-        mapper_y.setVisibility(VisibilityChecker.Std.defaultInstance().withFieldVisibility(JsonAutoDetect.Visibility.ANY));
-        PostYesterday posts_y = mapper.readValue(response_y.body(), PostYesterday.class);
-
-
-        Rates rubles = posts.rates;
-        String str = rubles.toString();
-        String str2 = str.replaceAll("\\s","");
-        float today = Float.parseFloat(str2);
-        System.out.println("Сегодня курс рубля к доллару: " + today);
-
-        Rates rubles_y = posts_y.rates;
-        String str_y = rubles_y.toString();
-        String str2_y = str_y.replaceAll("\\s","");
-        float yesterday = Float.parseFloat(str2_y);
-        System.out.println("Вчера курс рубля к доллару был: " + yesterday);
-
-        return whichisbigger(today, yesterday);
-    }
-
-    public static int whichisbigger(float today, float yesterday) {
-        if (today > yesterday)
-            return 1;
-        else
-            return 0;
-    }
-
     @GetMapping("/")
     public String rich(Model model) throws IOException, InterruptedException {
+        /**
+         * Контроллер. В зависимости от чисел выводим нужную страницу html с гифкой;
+         * REST API гифок - https://developers.giphy.com;
+         * В html-файлах я использовала фреймворк Bootstrap.
+         */
         float fin = 0;
-        fin = MainController.lala(fin);
+        fin = MainController.http(fin);
         if (fin == 1) {
             model.addAttribute("name", fin);
             return "poor";
@@ -94,4 +49,51 @@ public class MainController {
             return "rich";
         }
     }
+    public static int http(float fin) throws IOException, InterruptedException {
+
+        float today = func(POSTS_API_URL);
+        float yesterday = func(POSTS_API_URL_YESTERDAY);
+        return whichisbigger(today, yesterday);
+    }
+    public static float func(String API_URL) throws IOException, InterruptedException {
+        /**
+         * По смыслу разделю эту функцию на три части:
+         * 1. Нам нужно достать данные из https://openexchangerates.org
+         * 2. Преобразовываем http запрос в объект Post и Rates с информацией из json'a(это можно сделать проще через JSONObject, но я решила попробовать сделать более сложным путём и создать классы с переменными)
+         * 3. Когда мы получаем значения из RUB, нам нужно преобразовать из в float.
+         */
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .GET()
+                .header("accept", "application/json")
+                .uri(URI.create(API_URL))
+                .build();
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
+        mapper.setVisibility(VisibilityChecker.Std.defaultInstance().withFieldVisibility(JsonAutoDetect.Visibility.ANY));
+        Post posts = mapper.readValue(response.body(), Post.class);
+
+        Rates rubles = posts.rates;
+        String str = rubles.toString();
+        String str2 = str.replaceAll("\\s","");
+        float number = Float.parseFloat(str2);
+        System.out.println("Сегодня курс рубля к доллару: " + number);
+
+        return number;
+    }
+
+    public static int whichisbigger(float today, float yesterday) {
+        /**
+         * Сравниваем курс за сегодня и завтра.
+         */
+        if (today > yesterday)
+            return 1;
+        else
+            return 0;
+    }
+
+
 }
